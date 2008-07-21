4bc9a12bdea03d5e7fcb81e3e5f509d1aba96a34
Pedro Andrés Bonilla Polo <boninerd@gmail.com>
2008-07-21 16:54:20 -0500

    Corregido error en la migracion EstadoObras

--- db/migrate/20080721153558_add_estado_to_obras_ejemplares_and_disp_to_ejemplares.rb
+++ db/migrate/20080721153558_add_estado_to_obras_ejemplares_and_disp_to_ejemplares.rb
@@ -2,7 +2,7 @@ class AddEstadoToObrasEjemplaresAndDispToEjemplares < ActiveRecord::Migration
   def self.up
 		add_column :obras, :estado_obra_id, :integer, :default => 1,:null => false
 		add_column :ejemplares, :estado_ejemplar_id, :integer, :default => 1, :null => false
-		add_column :ejemplares, :disponibilidad_ejemplar_id, :default => 1, :integer, :null => false
+		add_column :ejemplares, :disponibilidad_ejemplar_id, :integer, :default => 1, :null => false
   end
 
   def self.down
